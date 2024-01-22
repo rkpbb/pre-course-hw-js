929d0baf6553e757d29e34800753fd8e187f6512
rkpbb <antiysdota@mail.ru>
2024-01-22 15:57:24 +0300

    Lesson2-03

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -1,8 +1,8 @@
 const giveMeRightNumbers = (value1, value2, maximum) => {
-    if (value1 === value2 || value1 <= maximum) {
+    if ((value1 <= maximum) && (value1 !== value2)) {
         console.log('Success!');
     } else {
-        console.log('Something wrong!');
+        console.log('Что-то не так!');
     }
 }
-giveMeRightNumbers(300, 100, 200)
+giveMeRightNumbers(100, 100, 200);

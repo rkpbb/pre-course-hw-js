ae63891f16179c8146f1767faec576e2c63b0f8d
rkpbb <antiysdota@mail.ru>
2024-01-26 16:09:35 +0300

    Lesson2-04

--- lesson-02/04.js
+++ lesson-02/04.js
@@ -1,5 +1,5 @@
 const giveMeRightCondition = (value1, value2, maximum) => {
-    if (value1 === value2 && value2 <= maximum) {
+    if (value1 === value2 && value1 <= maximum) {
         console.log('Success!');
     } else {
         console.log('Something wrong!');
